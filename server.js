8d9d63fe89c01f4bd764f5eb6a17fbb318ef0baf
Gradt <carolinefell@yandex.ru>
2020-05-30 15:24:54 +0300

    Rewrited the code 4

--- server.js
+++ server.js
@@ -1,101 +1,105 @@
 const http = require('http');
+const path = require('path');
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const koaStatic = require('koa-static');
-const path = require('path');
 const fs = require('fs');
-const uuidv4 = require('uuid/v4');
-
-const publicDir = path.join(__dirname, '/public');
-
+const uuid = require('uuid');
 const app = new Koa();
 
-app.use(koaBody({
-  urlencoded: true,
-  multipart: true,
-}));
-app.use(koaStatic(publicDir));
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback()).listen(port)
 
-const imagesSrcs = [];
-
-function generateIds(images) {
-  const isAllRenamed = [];
-  images.forEach((image) => {
-    const id = uuidv4();
-    const extension = image.name.match(/(?<=\.).+$/).toString();
-    const newName = `${id}.${extension}`;
-    const renamed = new Promise((res) => {
-      fs.copyFile(image.path, `${publicDir}/images/${newName}`, (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(image.path);
-          console.log(`${publicDir}/images/${newName}`);
-          console.log('Was added');
-          imagesSrcs.push(newName);
-          res(newName);
-        }
-      });
-    });
-    isAllRenamed.push(renamed);
-  });
-  return Promise.all(isAllRenamed);
-}
+const public = path.join(__dirname, '/public')
+app.use(koaStatic(public));
 
 app.use(async (ctx, next) => {
-  if (ctx.request.method === 'POST') {
-    let images = [];
-    if (ctx.request.files.file.length >= 2) {
-      images = Array.from(ctx.request.files.file);
-    } else {
-      images.push(ctx.request.files.file);
-    }
-    const justAddedImgs = await generateIds(images);
-    ctx.response.body = justAddedImgs;
-  }
-  if (ctx.request.method === 'GET') {
-    ctx.response.body = imagesSrcs;
-  }
-  if (ctx.request.method === 'DELETE') {
-    const imgToDel = ctx.request.url.match(/(?<=\?).+$/).toString();
-    const index = imagesSrcs.findIndex((item) => item === imgToDel);
-    imagesSrcs.splice(index, 1);
-    fs.unlink(`${publicDir}/images/${imgToDel}`, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Was deleted');
-      }
-    });
-    ctx.response.body = 'deleted';
-  }
   const origin = ctx.request.get('Origin');
   if (!origin) {
-    await next();
-    return;
+    return await next();
   }
-  const headers = { 'Access-Control-Allow-Origin': '*' };
+  const headers = { 'Access-Control-Allow-Origin': '*', };
+
   if (ctx.request.method !== 'OPTIONS') {
-    ctx.response.set({ ...headers });
+    ctx.response.set({...headers});
     try {
-      await next();
-      return;
+      return await next();
     } catch (e) {
-      e.headers = { ...e.headers, ...headers };
+      e.headers = {...e.headers, ...headers};
       throw e;
     }
   }
+
   if (ctx.request.get('Access-Control-Request-Method')) {
     ctx.response.set({
       ...headers,
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
     });
+
     if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
     }
     ctx.response.status = 204;
   }
 });
+  
+app.use(koaBody({
+  text: true,
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
 
-const port = process.env.PORT || 7070;
-http.createServer(app.callback()).listen(port);
+let images = [];
+
+app.use(async (ctx) => {
+ 
+  if (ctx.method === 'GET') {
+    ctx.response.body = images;
+  }
+
+ 
+  if (ctx.method === 'POST') {
+    const { name } = ctx.request;
+    const { file } = ctx.request.files;
+    const link = await new Promise((resolve, reject) => {
+      const oldPath = file.path;
+      const filename = file.name;
+      const newPath = path.join(public, filename);
+
+      const callback = (error) => reject(error);
+
+      const readStream = fs.createReadStream(oldPath);
+      const writeStream = fs.createWriteStream(newPath);
+
+      readStream.on('error', callback);
+      writeStream.on('error', callback);
+
+      readStream.on('close', () => {
+        console.log('close');
+        fs.unlink(oldPath, callback);
+        console.log(filename);
+        resolve(filename);
+      });
+      console.log(filename);
+      readStream.pipe(writeStream);
+    });
+    console.log(link);
+    images.push(`http://localhost:7070/${link}`)
+    ctx.response.body = link;
+    return;
+  };
+
+  if (ctx.method === 'DELETE') {
+    const { file } = ctx.request.query;
+    const fileName = path.parse(file).base;
+    images = images.filter((item) => item !== file);
+
+    fs.unlink(`${public}/${fileName}`, (err) => {
+      if (err) throw err;
+      console.log(`${public}/${fileName} was deleted`);
+    });
+    ctx.response.body = 'ok';
+    return;
+  }
+});
